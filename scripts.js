a994ab48f8ac349f44c0dad233c3f4f58390ce1e
domd11 <dominiquedesertb@gmail.com>
2022-10-02 10:02:28 -0400

    second commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 var displayShows = document.getElementById("display-shows"); 
 var displayMovies = document.getElementById("display-movies"); 
+var btnMovies = document.getElementById("link-text-movies"); 
+var btnShows = document.getElementById("link-text-shows"); 
+
+
 
 let localShows = JSON.parse(localStorage.getItem('localShow')); 
 let localMovies = JSON.parse(localStorage.getItem('localMovie')); 
+
 
 
 movies = localMovies; 
@@ -12,17 +17,37 @@ shows = localShows;
 const slicedMovies = movies.slice(0,5); 
 const slicedShows = shows.slice(0,5); 
 
-let movie = ''; 
-let show = ''; 
+if (slicedMovies.length < 1){
+    const noMovies = `<h3 class="no-content">You do not have any movies. Go add some!</h3>`;
+    displayMovies.innerHTML = noMovies; 
+    btnMovies.innerHTML = "Add Movies"; 
+}
 
-slicedMovies.forEach((data) => {
-    movie += `<tr><td>${data.title}</td><td>${data.genre}</td><td>${data.rating}</td><td>${data.date}</td></tr>`
-}); 
+if (slicedShows < 1){
+    const noShows = `<h3 clas="no-content">You do have have any movies. Go add some!</h3>`;
+    displayShows.innerHTML = noShows;  
+    btnShows.innerHTML = "Add Shows"; 
+}
 
-slicedShows.forEach((data) => {
-    show += `<tr><td>${data.title}</td><td>${data.genre}</td><td>${data.rating}</td><td>${data.date}</td></tr>`
-}); 
 
-displayMovies.innerHTML = movie; 
-displayShows.innerHTML = show; 
+if (slicedMovies.length > 1){
+    let movie = ''; 
+    slicedMovies.forEach((data) => {
+        movie += `<tr><td>${data.title}</td><td>${data.genre}</td><td>${data.rating}</td><td>${data.date}</td></tr>`
+    }); 
+    displayMovies.innerHTML = movie; 
+
+} 
+
+if (slicedShows.length > 1) {
+    let show = ''; 
+
+    slicedShows.forEach((data) => {
+        show += `<tr><td>${data.title}</td><td>${data.genre}</td><td>${data.rating}</td><td>${data.date}</td></tr>`
+    }); 
+
+    displayShows.innerHTML = show; 
+
+}
+
 
